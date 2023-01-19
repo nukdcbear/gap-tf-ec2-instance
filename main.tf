72c7829f5119ebfb1b10409de643e73f0d66d841
David Barringer <david.barringer@happify.com>
2023-01-19 16:43:12 -0500

    Update random server name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "random_pet" "server" {
-  length = 3
+  length = 2
   keepers = {
     # Generate a new pet name each time we switch to a new AMI id
     ami_id = data.aws_ami.ubuntu.id
